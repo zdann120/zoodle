3dd671a4f724ce343b3ef83033b9b9700964c83e
Zach Nusbaum <nusbaumz@msu.edu>
2016-02-08 20:35:10 -0700

    hide dashboard if logged in as admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,12 +1,15 @@
 ActiveAdmin.register_page "Dashboard" do
 
   #menu true
+  if user_signed_in?
+    menu false
+  end
 
   content title: "Zoodle Admin" do
     if user_signed_in?
         if current_user.admin
             p "You have admin access."
-            
+
         else
             p "You do not have admin access."
         end
