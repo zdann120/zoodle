c59a469c5ccf5af309775dfb0ca45dee4309d44b
Zach Nusbaum <nusbaumz@msu.edu>
2016-02-08 20:28:41 -0700

    authorization works.  i made a dumb mistake.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,11 +1,12 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu false
+  #menu true
 
   content title: "Zoodle Admin" do
     if user_signed_in?
         if current_user.admin
             p "You have admin access."
+            
         else
             p "You do not have admin access."
         end
